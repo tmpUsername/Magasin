ec379957a93a511e90b9b92c9453c2295995e3ea
tmpUsername <admin@fddf-PC>
2016-09-21 15:49:19 +0200

    Categorie.java: ajout de la liste des produits

--- src/magasin/entity/Categorie.java
+++ src/magasin/entity/Categorie.java
@@ -6,6 +6,9 @@
 package magasin.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
@@ -13,6 +16,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -26,7 +30,17 @@ public class Categorie implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nom;
-    
+    @OneToMany(mappedBy = "categorie")
+    private List<Produit> produits = new ArrayList<>();
+
+    public List<Produit> getProduits() {
+        return  produits;
+    }
+
+    public void setProduits(ArrayList<Produit> produits) {
+        this.produits = produits;
+    }
+
 
     public String getNom() {
         return nom;
