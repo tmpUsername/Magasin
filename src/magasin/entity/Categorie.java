6124ffb5f1c8889cdcac9be1c768c9e99e71f94d
tmpUsername <admin@fddf-PC>
2016-09-21 12:04:21 +0200

    Categorie.java: ajout des accesseurs

--- src/magasin/entity/Categorie.java
+++ src/magasin/entity/Categorie.java
@@ -24,6 +24,14 @@ public class Categorie implements Serializable {
     private Long id;
     private String nom;
     
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
     public Long getId() {
         return id;
     }
