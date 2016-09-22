c12ed6b5351b989ef7d238231e5c967d9c94f15a
tmpUsername <admin@fddf-PC>
2016-09-22 10:26:02 +0200

    MagasinDBTest.java: Correction du Test avec suppression de la base

--- test/magasin/entity/MagasinDBTest.java
+++ test/magasin/entity/MagasinDBTest.java
@@ -7,9 +7,12 @@ package magasin.entity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -20,48 +23,65 @@ public class MagasinDBTest {
     @Before
     public void affectation() {
         //vide toutes les tables
-        
-        //ajoute des données
-        
         EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
 
         em.getTransaction().begin();
+        Query query = em.createQuery("DELETE FROM Produit p");
+        query.executeUpdate();
+        
+        em.createQuery("DELETE FROM Categorie c").executeUpdate();
+        em.createQuery("DELETE FROM Commande c").executeUpdate();
+        em.createQuery("DELETE FROM Client c").executeUpdate();
+        
+        
+        em.getTransaction().commit();
+        
+        //ajoute des données
+        em.getTransaction().begin();
         Categorie cat = new Categorie();
         cat.setNom("Basket");
+        cat.setId(1L);
         em.persist(cat);
 
         Categorie cat2 = new Categorie();
         cat2.setNom("Lunette de soleil");
+        cat2.setId(2L);
         em.persist(cat2);
 
         Produit prod = new Produit();
         prod.setTitre("Ray-ban");
         prod.setCategorie(cat2);
+        prod.setId(1L);
         em.persist(prod);
         em.getTransaction().commit();
         
         em.getTransaction().begin();
         Client cli = new Client();
+        cli.setId(1L);
         cli.setLogin("Michel");
         em.persist(cli);
 
         Client cli2 = new Client();
         cli2.setLogin("Louis");
+        cli2.setId(2L);
         em.persist(cli2);
 
         Commande com = new Commande();
         com.setClient(cli2);
+        com.setId(1L);
         em.persist(com);
         em.getTransaction().commit();
     }
 
-    //@Test
+    @Test
     public void testListeProdCategorie() {
         EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
 
-        Categorie cat = em.find(Categorie.class, 2L);
+        Categorie cat = em.find(Categorie.class, 1L);
         for (Produit p : cat.getProduits()) {
-            System.out.println(p);
+            if(!p.getTitre().equals("Basket")){
+                Assert.fail("l'élèment Basket n'est pas trouvé!");
+            }
         }
     }
 
@@ -69,9 +89,11 @@ public class MagasinDBTest {
     public void testListeClientCommande() {
         EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
 
-        Client cl = em.find(Client.class, 2L);
+        Client cl = em.find(Client.class, 1L);
         for (Commande c : cl.getCommandes()) {
-            System.out.println(c);
+            if (!c.getClient().getLogin().equals("Michel")){
+                Assert.fail("On a pas le client Michel pour la commande");
+            }
         }
     }
 }
